206ae3fda7f453d4e05e6713d2c2c962f6759ebf
user-MP <119966448+user-MP@users.noreply.github.com>
2023-04-21 20:02:46 +0300

    Add files via upload

---
+++ 1-0-rdbms-and-sql/1-2-1-one-to-one-schema/target/classes/db/migration/table_initialization.sql
@@ -0,0 +1,51 @@
+/*
+
+User profile database stores information about users and their work profiles.
+
+Each user has one and only one work profile.
+
+Each user has stored first and last names, email and birthday which are mandatory. Email is a unique value.
+A profile for each user is optional, and consists of optional information: city, job_position, company and education.
+All these fields are regular strings without restrictions.
+
+  TECH NOTES AND NAMING CONVENTION
+- All tables, columns and constraints are named using "snake case" naming convention
+- All table names must be plural (e.g. "companies", not "company")
+- All tables (except link tables) should have a single-value identifier of type BIGINT, which is a primary key
+- All primary key, foreign key, and unique constraint should be named according to the naming convention.
+- All "1 - optional 1" relations should be handled using the same primary key value for both tables. E.g. child table
+should have a column that stores primary key from a parent table, which is a foreign key and primary key at the same time
+
+- All primary keys should be named according to the following rule "table_name_PK"
+- All foreign keys should be named according to the following rule "table_name_reference_table_name_FK"
+- All alternative keys (unique) should be named according to the following rule "table_name_column_name_AK"
+
+*/
+
+-- TODO: implement the SQL according to the description
+
+
+
+CREATE TABLE IF NOT EXISTS users(
+    id BIGINT,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    email  varchar(255) NOT NULL,
+    birthday date NOT NULL ,
+    CONSTRAINT users_PK PRIMARY KEY (id),
+    CONSTRAINT users_email_AK UNIQUE(email)
+
+
+);
+
+CREATE TABLE IF NOT EXISTS profiles(
+    user_id BIGINT,
+    city varchar(255),
+    job_position varchar(255),
+    company varchar(255),
+    education varchar(255),
+    CONSTRAINT profiles_PK PRIMARY KEY (user_id),
+    CONSTRAINT profiles_users_FK FOREIGN KEY(user_id) REFERENCES users
+
+
+);
